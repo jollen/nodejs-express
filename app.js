5d3ce0aa8beb5704e28266ebf36e89d1f40a6339
Jollen Chen <jollen@moko365.com>
2013-11-02 14:28:41 +0800

    新增 8.4 節的練習

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var hello = require('./routes/hello');
 
 var app = express();
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/hello', hello.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
