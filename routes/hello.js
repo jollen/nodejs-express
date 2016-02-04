a4f04668b8cf8ac4958098f71b3389213f00606e
jollen <jollen@moko365.com>
2016-02-04 12:03:32 +0800

    upgrade to Express 4.x

--- routes/hello.js
+++ routes/hello.js
@@ -1,8 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
+/* GET users listing. */
+router.get('/', function(req, res, next) {
   res.render('hello');
-};
+});
+
+module.exports = router;
